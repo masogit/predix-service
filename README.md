008bd2bedb9034642ef75264f9456ece01c370c4
Man, Shuo <shuo.man@gmail.com>
2017-04-04 00:50:05 +0800

    Test predix server successfully

---
+++ README.md
@@ -0,0 +1,14 @@
+# Predix Node Service
+
+A most simple nodejs (express) rest service on Predix (Cloud Foundry)
+
+**url**
+
+`https://my-node.run.aws-jp01-pr.ice.predix.io`
+
+**return**
+```
+{
+  "test": "abc"
+}
+```
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const server = express()
+const port = process.env.PORT || 3000
 
 server.use('*', (req, res) => {
   res.json({
@@ -7,4 +8,4 @@ server.use('*', (req, res) => {
   })
 })
 
-server.listen(8088, () => console.log('Express Server started on 8088'))
+server.listen(port, () => console.log(`Express Server started on ${port}`))
