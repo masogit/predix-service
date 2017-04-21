db965525c47a0ff87d529020338d00cec05a0bb4
Man, Shuo <shuo.man@gmail.com>
2017-04-21 17:55:27 +0800

    use compression

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const httpProxy = require('http-proxy');
-const path = require('path');
-const server = express();
+const express = require('express')
+const compression = require('compression')
+const httpProxy = require('http-proxy')
+const path = require('path')
+const server = express()
 const port = process.env.PORT || 8080
 const remote = "http://apm-uat.hcdigital.com.cn:8090"
 
-const proxy = httpProxy.createProxyServer({secure: false});
+const proxy = httpProxy.createProxyServer({secure: false})
 proxy.on('error', function(e) {
-  console.log(e);
-});
+  console.log(e)
+})
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  console.log('proxyReq');
-  console.log(options);
-});
+  console.log('proxyReq')
+  console.log(options)
+})
 
-server.use(express.static(path.join(__dirname, '/dist')));
+server.use(compression())
+server.use(express.static(path.join(__dirname, '/dist')))
 
 server.use('/gateway', (req, res) => {
-  proxy.web(req, res, { target: remote, pathRewrite: {'^/gateway' : ''}, changeOrigin: true });
+  proxy.web(req, res, { target: remote, pathRewrite: {'^/gateway' : ''}, changeOrigin: true })
 })
 
 server.use('*', function (req, res, next) {
   res.sendFile(path.join(__dirname + '/dist/index.html'))
-});
+})
 
 
 server.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
-});
+})
